8c74e360a79ae448502dd18152a4188c914694ff
qinlei <qinl@isvtop.com>
2025-11-06 21:43:27 +0800

    feat：gateway和auth模块包路径变更

---
+++ bubble-auth/src/main/java/com/bubblecloud/auth/BubbleAuthApplication.java
@@ -0,0 +1,23 @@
+package com.bubblecloud.auth;
+
+import com.pig4cloud.pig.common.feign.annotation.EnablePigFeignClients;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 认证授权中心应用启动类
+ *
+ * @author lengleng
+ * @date 2025/05/30
+ */
+@EnablePigFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BubbleAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleAuthApplication.class, args);
+	}
+
+}
---
+++ bubble-gateway/src/main/java/com/bubblecloud/gateway/config/GatewayConfiguration.java
@@ -0,0 +1,37 @@
+package com.bubblecloud.gateway.config;
+
+import com.bubblecloud.gateway.filter.PigRequestGlobalFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.bubblecloud.gateway.handler.GlobalExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 网关配置类
+ *
+ * @author qinlei
+ * @date 2025/05/30
+ */
+@Configuration(proxyBeanMethods = false)
+public class GatewayConfiguration {
+
+	/**
+	 * 创建PigRequest全局过滤器
+	 * @return PigRequest全局过滤器
+	 */
+	@Bean
+	public PigRequestGlobalFilter pigRequestGlobalFilter() {
+		return new PigRequestGlobalFilter();
+	}
+
+	/**
+	 * 创建全局异常处理程序
+	 * @param objectMapper 对象映射器
+	 * @return 全局异常处理程序
+	 */
+	@Bean
+	public GlobalExceptionHandler globalExceptionHandler(ObjectMapper objectMapper) {
+		return new GlobalExceptionHandler(objectMapper);
+	}
+
+}
