9e91caa10296f07ca87e186f2f3fbe6ff07b6aa7
qinlei <qinl@isvtop.com>
2025-11-06 22:55:12 +0800

    feat：redis缓存key统一加上前缀

--- bubble-common/bubble-common-core/src/main/java/com/bubblecloud/common/core/constant/CacheConstants.java
+++ bubble-common/bubble-common-core/src/main/java/com/bubblecloud/common/core/constant/CacheConstants.java
@@ -9,43 +9,47 @@ package com.bubblecloud.common.core.constant;
 public interface CacheConstants {
 
 	/**
+	 * 每个项目定义一个顶级目录
+	 */
+	String TOP = "bubble::";
+	/**
 	 * oauth 缓存前缀
 	 */
-	String PROJECT_OAUTH_ACCESS = "token::access_token";
+	String PROJECT_OAUTH_ACCESS = TOP + "token::access_token";
 
 	/**
 	 * 验证码前缀
 	 */
-	String DEFAULT_CODE_KEY = "DEFAULT_CODE_KEY:";
+	String DEFAULT_CODE_KEY = TOP + "DEFAULT_CODE_KEY:";
 
 	/**
 	 * 菜单信息缓存
 	 */
-	String MENU_DETAILS = "menu_details";
+	String MENU_DETAILS = TOP + "menu_details";
 
 	/**
 	 * 用户信息缓存
 	 */
-	String USER_DETAILS = "user_details";
+	String USER_DETAILS = TOP + "user_details";
 
 	/**
 	 * 字典信息缓存
 	 */
-	String DICT_DETAILS = "dict_details";
+	String DICT_DETAILS = TOP + "dict_details";
 
 	/**
 	 * 角色信息缓存
 	 */
-	String ROLE_DETAILS = "role_details";
+	String ROLE_DETAILS = TOP + "role_details";
 
 	/**
 	 * oauth 客户端信息
 	 */
-	String CLIENT_DETAILS_KEY = "client:details";
+	String CLIENT_DETAILS_KEY = TOP + "client:details";
 
 	/**
 	 * 参数缓存
 	 */
-	String PARAMS_DETAILS = "params_details";
+	String PARAMS_DETAILS = TOP + "params_details";
 
 }
