6781b305555147b2d36e8018fb39d025f657ed07
qinlei <qinl@isvtop.com>
2025-11-10 21:46:08 +0800

    feat：OA模块启动环境配置

--- bubble-biz/bubble-biz-oa/src/main/java/com/bubblecloud/biz/oa/BubbleOaApplication.java
+++ bubble-biz/bubble-biz-oa/src/main/java/com/bubblecloud/biz/oa/BubbleOaApplication.java
@@ -1,13 +1,23 @@
 package com.bubblecloud.biz.oa;
 
+import com.bubblecloud.common.security.annotation.EnableCustomResourceServer;
+import com.bubblecloud.common.swagger.annotation.EnableCustomDoc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
- * Hello world!
+ * OA模块启动类
  *
+ * @author Rampart Qin
+ * @date 2020/3/26 下午2:57
  */
-public class BubbleOaApplication
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@EnableCustomDoc("oa")
+@EnableCustomResourceServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BubbleOaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleOaApplication.class, args);
+	}
 }
