57a56c1e00b8ae7e3e447ebbb97b685c9e0ec0f0
qinlei <qinl@isvtop.com>
2025-11-10 22:01:10 +0800

    feat：新增工作流模块启动配置

--- bubble-biz/bubble-biz-flow/src/main/java/com/bubblecloud/biz/flow/BubbleFlowApplication.java
+++ bubble-biz/bubble-biz-flow/src/main/java/com/bubblecloud/biz/flow/BubbleFlowApplication.java
@@ -1,13 +1,23 @@
 package com.bubblecloud.biz.flow;
 
+import com.bubblecloud.common.security.annotation.EnableCustomResourceServer;
+import com.bubblecloud.common.swagger.annotation.EnableCustomDoc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
- * Hello world!
+ * 工作流模块启动类
  *
+ * @author Rampart Qin
+ * @date 2020/3/26 下午2:57
  */
-public class BubbleFlowApplication
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@EnableCustomDoc("flow")
+@EnableCustomResourceServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BubbleFlowApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleFlowApplication.class, args);
+	}
 }
