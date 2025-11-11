416bc5447e46d05ea0f4bd7cfb6913613397a2ea
qinlei <qinl@isvtop.com>
2025-11-11 22:20:20 +0800

    feat：新增oa和flow模块

---
+++ bubble-biz/bubble-biz-flow/src/main/java/com/bubblecloud/biz/flow/BubbleFlowApplication.java
@@ -0,0 +1,23 @@
+package com.bubblecloud.biz.flow;
+
+import com.bubblecloud.common.security.annotation.EnableCustomResourceServer;
+import com.bubblecloud.common.swagger.annotation.EnableDoc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 工作流模块启动类
+ *
+ * @author Rampart Qin
+ * @date 2020/3/26 下午2:57
+ */
+@EnableDoc("flow")
+@EnableCustomResourceServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BubbleFlowApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleFlowApplication.class, args);
+	}
+}
