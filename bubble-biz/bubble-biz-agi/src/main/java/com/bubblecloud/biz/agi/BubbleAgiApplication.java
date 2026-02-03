d2468c104e32a9a3693bd618df1b86e11553e620
qinlei <qinl@isvtop.com>
2026-02-03 09:39:34 +0800

    feat：新增bubble-api-agi 和bubble-biz-agi 业务模块

---
+++ bubble-biz/bubble-biz-agi/src/main/java/com/bubblecloud/biz/agi/BubbleAgiApplication.java
@@ -0,0 +1,23 @@
+package com.bubblecloud.biz.agi;
+
+import com.bubblecloud.common.security.annotation.EnableCustomResourceServer;
+import com.bubblecloud.common.swagger.annotation.EnableDoc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * AGI模块启动类
+ *
+ * @author Rampart Qin
+ * @date 2020/3/26 下午2:57
+ */
+@EnableDoc("agi")
+@EnableCustomResourceServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BubbleAgiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleAgiApplication.class, args);
+	}
+}
